809aff8232d321bab08558ba98d632256a7493f4
Adriaan de Groot <groot@kde.org>
2017-07-08 12:11:31 -0400

    Also find and link to libs

--- ceci/cmake/FindErlang.cmake
+++ ceci/cmake/FindErlang.cmake
@@ -23,11 +23,26 @@ find_path(_erl_dir erl_interface.h)
 
 if(_ei_dir AND _erl_dir)
     if(_ei_dir STREQUAL _erl_dir)
-        SET(Erlang_INCLUDE_DIRS ${_ei_dir})
+        set(Erlang_INCLUDE_DIRS ${_ei_dir})
     else()
-        SET(Erlang_INCLUDE_DIRS ${_ei_dir} ${_erl_dir})
+        set(Erlang_INCLUDE_DIRS ${_ei_dir} ${_erl_dir})
     endif()
+    message(STATUS " .. includes ${_ei_dir}")
 endif()
+
+find_library(_ei_lib
+    NAMES ei
+)
+find_library(_erl_lib
+    NAMES erl_interface
+)
+if (_ei_lib AND _erl_lib)
+    # These are generally static, and erl_ derpend on ei,
+    # so take care with the order.
+    set(Erlang_LIBRARIES ${_erl_lib} ${_ei_lib} pthread)
+    message(STATUS " .. libraries ${_ei_lib} ${_erl_lib}")
+endif()
+
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Erlang
@@ -36,4 +51,5 @@ find_package_handle_standard_args(Erlang
     REQUIRED_VARS
         Erlang_EXECUTABLE
         Erlang_INCLUDE_DIRS
+        Erlang_LIBRARIES
 )

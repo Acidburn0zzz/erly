d9a0cca26731b2f13c9433ebfbe47afc06c20a73
Adriaan de Groot <groot@kde.org>
2017-07-10 04:11:35 -0400

    CECI: initialise erl_interface before use

--- ceci/src/ceci.c
+++ ceci/src/ceci.c
@@ -1,14 +1,38 @@
+#include <string.h>
+
 #include "erl_interface.h"
 #include "ei.h"
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	int sockfd;
-	char *nodename="bilbo@beastie"; /* An example */
-	if ((sockfd = erl_connect(nodename)) < 0)
+	char servername[128];
+	const char *hostname = "localhost";
+	int offset;
+
+	strcpy(servername, "messenger@");
+	offset = strlen(servername);
+
+	if ((argc > 1) && (argv[1]))
 	{
-		erl_err_quit("ERROR: erl_connect failed");
-		return 1;
+		hostname = argv[1];
 	}
+	strncpy(servername + offset, hostname, sizeof(servername) - offset);
+	servername[sizeof(servername)-1] = 0;
+
+	erl_init(NULL, 0);
+
+	if (!erl_connect_init(177, NULL, 0))
+	{
+		erl_err_quit("ERROR: erl_connect_init failed.");
+		return 1;  /* NOTREACHED */
+	}
+
+	if ((sockfd = erl_connect(servername)) < 0)
+	{
+		erl_err_quit("ERROR: erl_connect failed for %s", servername);
+		return 1;  /* NOTREACHED */
+	}
+
 	return 0;
 }
